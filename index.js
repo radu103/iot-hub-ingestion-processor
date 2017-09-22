20b41a96ece574cf680272604172c6d7b7bad0a3
Travel Devel <traveldevel@hotmail.com>
2017-09-23 00:52:39 +0300

    save to mongo should work but the device is not found yet

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 
 // required modules
+const cfenv = require('cfenv');
 const async = require('async');
 const kafkaNode = require('kafka-node');
 const ConsumerGroup = require('kafka-node').ConsumerGroup;
+const mongoClient = require('mongodb').MongoClient;
 
 // get ENV vars from CF
 const landscapeName = process.env.LANDSCAPE_NAME;
@@ -12,7 +14,35 @@ const tenantName = process.env.TENANT_NAME;
 const zookeeperHost = process.env.ZOOKEEPER_HOST;
 const zookeeperPort = process.env.ZOOKEEPER_PORT;
 
-// connect client
+// mongo create url
+// configs from env vars
+var appEnv = cfenv.getAppEnv();
+//console.log(appEnv.getServices());
+
+var mongoServiceName = "iot_hub_mongo_" + landscapeName;
+var mongoService = appEnv.getService(mongoServiceName);
+var mongoCredentials = appEnv.getServiceCreds(mongoServiceName);
+var mongoUrl = mongoCredentials.uri;
+
+console.log(mongoServiceName + " found in VCAP_SERVICES");
+console.log(mongoService.credentials);
+
+var mongoDbName = '';
+var mongoUrl = '';
+
+if(mongoService !== undefined){
+
+    mongoUrl = mongoService.credentials.uri + "?ssl=false";
+
+    var mongodbUri = require('mongodb-uri');
+    var uriObject = mongodbUri.parse(mongoUrl);
+    mongoDbName = uriObject.database;
+}
+
+console.log("Mongo url : ", mongoUrl);
+console.log("Mongo db : ", mongoDbName);
+
+// zookeeper connect client
 var zookeeper = require('node-zookeeper-client');
 var client = zookeeper.createClient(zookeeperHost + ':' + zookeeperPort);
 
@@ -86,6 +116,53 @@ function onMessage(message) {
     
     var msg = JSON.parse(message.value);
     console.log('Message : ', msg);
+
+    // connect to mongo and put raw data there
+    mongoClient.connect(mongoUrl, function(err, db){
+
+        var deviceCol = db.collection(tenantName + "_device");        
+        deviceCol.findOne({_id : msg.device_id}, function(device){
+
+            if(device !== undefined && device !== null){
+                console.log('device found : ', device);
+
+                var project_id = null;
+                var group_id = null;
+
+                var rawData = {
+                    'project_id' : project_id,
+                    'group_id' : group_id,
+                    'device_id' : msg.device_id,
+                    'values' : msg.values,
+                    'recorded_time' : new Date(),
+                    'created_at' : new Date()
+                };
+        
+                // Get the raw data collection collection 
+                var rawDataCol = db.collection(tenantName + "_raw_data");
+                
+                // Insert some documents 
+                rawDataCol.insertOne([
+                    rawData
+                ], function(err, result) {
+        
+                    if(err){
+                        console.log('mongo err : ', err);
+                    }
+        
+                    console.log(result);
+        
+                    db.close();
+                });      
+            }
+            else
+            {
+                console.log('device not found : ', msg.device_id);
+                db.close();
+            }  
+        });
+
+    });
 }
 
 // close all consumer groups on exit
